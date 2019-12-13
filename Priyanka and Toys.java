e406a7f063eef810bfd9e1f461c3387e4dd050ac
Atharva Kulkarni <atharvakulkarni2204@gmail.com>
2019-12-13 16:07:13 +0530

    Create Priyanka and Toys.java

---
+++ Priyanka and Toys.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class pat
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int w[] = new int[n];
+
+        for(int i=0;i<n;i++)
+            w[i]=sc.nextInt();
+
+        Arrays.sort(w);
+        int b = -1,ctr=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(w[i] > b)
+            {
+                b = w[i]+4;
+                ctr++;
+            }
+        }
+        System.out.println(ctr);
+    }
+}
