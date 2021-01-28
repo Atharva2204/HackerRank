48ee8202f0415e2d09afb3ee37378923b288063f
Atharva Kulkarni <atharvakulkarni2204@gmail.com>
2021-01-29 03:01:35 +0530

    Create String Construction.java

---
+++ Problem Solving/String Construction.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class sc
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();        
+        
+        for(int i = 0; i < n; i++)
+        {
+            String s = sc.next();
+            
+            Set<Character> uniqueChars = new HashSet<>();
+            for(char c : s.toCharArray())
+            {
+                uniqueChars.add(c);
+            }
+            System.out.println(uniqueChars.size());
+        }
+    }
+}
